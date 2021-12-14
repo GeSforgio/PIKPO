5dbe62f31d242900435410f348aacbf805cbab8a
PIDOR <kirillyakovlev10@gmail.com>
2021-12-15 01:42:24 +0500

    Дока для запуска

--- app.py
+++ app.py
@@ -6,34 +6,35 @@ from sqlalchemy import create_engine, text
 app = Flask(__name__)
 
 
-
 @app.route('/')
-def ind():
+def main():
     conn = create_engine("postgresql+psycopg2://movie:movie@localhost:31000/movie")
     sql = text(
         open("views/sql_all", encoding="utf8").read()
     )
-    result=conn.execute(sql)
+    result = conn.execute(sql)
     list = []
     for rowproxy in result:
         movie_dict = dict(rowproxy)
         list.append(movie_dict)
-    #list=get_anime()
-    return render_template('main.html',table=list)
+    return render_template('main.html', table=list)
 
-@app.route('/search',methods=['POST'])
+
+@app.route('/search', methods=['POST'])
 def search():
     conn = create_engine("postgresql+psycopg2://movie:movie@localhost:31000/movie")
-    kok=str(request.form.get("comment"))
+    input_value = str(request.form.get("comment"))
     sql = text(
         open("views/sql_search", encoding="utf8").read()
     )
-    result = conn.execute(sql,string =kok)
+    result = conn.execute(sql, string=input_value)
     list = []
     for rowproxy in result:
         movie_dict = dict(rowproxy)
         list.append(movie_dict)
-    # list=get_anime()
+
     return render_template('main.html', table=list)
+
+
 if __name__ == '__main__':
-    app.run(host="localhost",port=6666)
+    app.run(host="localhost", port=6666)
